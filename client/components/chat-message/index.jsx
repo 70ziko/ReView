fe41e612437aaaf5645dfb999c9f3e5ed2a044e7
Emil Beski <beskiemil@gmail.com>
2025-03-09 03:48:48 +0100

    feat: added typing indicator and improve scrolling behavior

--- client/components/chat-message/index.jsx
+++ client/components/chat-message/index.jsx
@@ -1,8 +1,9 @@
 import { Card } from '../ui/card';
 import { Text } from '../ui/text';
 import { tva } from '@gluestack-ui/nativewind-utils/tva';
+import { useEffect, useState } from 'react';
 
-export const ChatMessage = ({ message: { author, time, content } }) => {
+export const ChatMessage = ({ typing, message: { author, time, content } }) => {
   const cardStyle = tva({
     slots: {
       base: 'flex-col rounded-bl-lg rounded-br-lg rounded-tl-none rounded-tr-lg gap-2',
@@ -20,14 +21,40 @@ export const ChatMessage = ({ message: { author, time, content } }) => {
 
   const { base, text } = cardStyle({ user: author === 'user' });
 
+  const [dots, setDots] = useState('.');
+
+  useEffect(() => {
+    let interval = null;
+    if (typing) {
+      interval = setInterval(() => {
+        setDots((prev) => {
+          if (prev === '...') {
+            return '.';
+          }
+          return prev + '.';
+        });
+      }, 200);
+    }
+    return () => {
+      if (typing) clearInterval(interval);
+    };
+  }, [typing]);
+
   return (
     <Card size={'lg'} className={base({ class: '' })}>
       <Text size={'sm'} className={text()}>
         {time}
       </Text>
-      <Text size={'lg'} className={text()}>
-        {content}
-      </Text>
+      {typing && (
+        <Text size={'lg'} className={text()}>
+          {dots}
+        </Text>
+      )}
+      {content && (
+        <Text size={'lg'} className={text()}>
+          {content}
+        </Text>
+      )}
     </Card>
   );
 };
